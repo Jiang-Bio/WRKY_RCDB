05349dd7019fe7f2b8a1281ce461b4486015a746
JIANGDK <jiangdk_bio@163.com>
2025-09-15 22:51:31 +0800

    Update figure3G_EnrichInRNA_ATAC_ATI.R

--- Figure3/figure3G_EnrichInRNA_ATAC_ATI.R
+++ Figure3/figure3G_EnrichInRNA_ATAC_ATI.R
@@ -1,12 +1,31 @@
-  ##3G RNA####
-  {
+#####################################################################
+##3G RNA####
+#####################################################################
+  rename_duplicates <- function(strings) {
+    counts <- table(strings)
+    seen <- setNames(rep(1, length(counts)), names(counts))
+    new_strings <- character(length(strings))
     
-    df2 <- read.table("~/Cotyledon_Root_RNASeq.count",header = T)[,c(9:10)]
-    df1 <- read.table("~/RNAseq.count",header = T)[,c(1:14)]
-    df <- cbind(df1,df2) 
+    for (i in seq_along(strings)) {
+      current_string <- strings[i]
+      if (counts[current_string] > 1) {
+        seen_count <- seen[current_string]
+        new_strings[i] <- paste(current_string, seen_count, sep = "_")
+        seen[current_string] <- seen_count + 1
+      } else {
+        new_strings[i] <- paste(current_string, 1, sep = "_")
+      }
+    }
+    
+    return(new_strings)
+  }
+  
+    #load gene raw count file
+    df <- read.table("~/diff_tissue_RNASeq.count",header = T)
     count_table<- df[,c(7:16)]
     colnames(count_table) <- c("Cotyledon","Cotyledon","Flower","Flower","Silique","Silique","Stem","Stem","Root","Root" ) %>% rename_duplicates()
-    rnaseq_tpm <-  t(t(expr1)/colSums(expr1))*10^6
+    #TPM calculation
+    rnaseq_tpm <-  t(t(count_table)/colSums(count_table))*10^6
     rnaseq_tpm <- cbind(df[,1:6],rnaseq_tpm)
     rnaseq_tpm$Geneid <- gsub(".Araport11.447",replacement = "",rnaseq_tpm$Geneid)
     
@@ -26,11 +45,14 @@
       library(motifmatchr)
       library(fjComm)
       pseudo=1000
-      
-      up_gene <- rnaseq_tpm[order(rnaseq_tpm[,x+6],decreasing = T),1][1:round(nrow(rnaseq_tpm)*0.10)]
-      down_gene <- rnaseq_tpm[order(rnaseq_tpm[,x+6],decreasing = F),1][1:round(nrow(rnaseq_tpm)*0.10)]
+      top=0.10
+      bottom=0.10
+      upstream = 300
+      downstream = 300
+      up_gene <- rnaseq_tpm[order(rnaseq_tpm[,x+6],decreasing = T),1][1:round(nrow(rnaseq_tpm)*top)]
+      down_gene <- rnaseq_tpm[order(rnaseq_tpm[,x+6],decreasing = F),1][1:round(nrow(rnaseq_tpm)*bottom)]
       library(TxDb.Athaliana.BioMart.plantsmart51)
-      promoter <- promoters(TxDb.Athaliana.BioMart.plantsmart51,upstream = 300,downstream = 300) %>% GenomicRanges::trim() %>% {.[width(.)==600]}
+      promoter <- promoters(TxDb.Athaliana.BioMart.plantsmart51,upstream = upstream,downstream = downstream) %>% GenomicRanges::trim() %>% {.[width(.)==downstream+upstream]}
       promoter$tx_name <- gsub("\\.[0-9]+","",promoter$tx_name)
       
       up_gene <- promoter[which(promoter$tx_name %in% up_gene)] 
@@ -44,13 +66,13 @@
       down_seq <- getSeq(BSgenome.Athaliana.TAIR.TAIR9::Athaliana,down_gene)
       
       
-      df <- motifmatchr::matchMotifs(pwms = pwm,subject = up_seq,out ="matches",p.cutoff=5e-05,bg="subject")
+      df <- motifmatchr::matchMotifs(pwms = pwm,subject = up_seq,out ="matches",p.cutoff=1e-05,bg="subject")
       match <- df@assays@data@listData$motifMatches %>% colSums()
       up_data <- match %>% as.data.frame() %>% `colnames<-`("value")
       up_data$"group" <-  colnames(rnaseq_tpm)[x+6]; up_data$"motif" <- names(pwm.l)#;up_data$"class" <- "up"
 
       
-      df <- motifmatchr::matchMotifs(pwms = pwm,subject = down_seq,out ="matches",p.cutoff=5e-05,bg="subject")
+      df <- motifmatchr::matchMotifs(pwms = pwm,subject = down_seq,out ="matches",p.cutoff=1e-05,bg="subject")
       match <- df@assays@data@listData$motifMatches %>% colSums()
       down_data <- match %>% as.data.frame() %>% `colnames<-`("value")
       down_data$"group" <- colnames(rnaseq_tpm)[x+6]; down_data$"motif" <- names(pwm.l)#;down_data$"class" <- "down"
@@ -60,22 +82,20 @@
     stopImplicitCluster()
     stopCluster(cl)
     
-    a <- bind_rows(result) 
+    dat <- bind_rows(result) 
 
-    a <- mutate(a,motif = fct_relevel(motif, "class11", "class10", "class9", 
+    dat <- mutate(dat,motif = fct_relevel(motif, "class11", "class10", "class9", 
                                       "class8", "class7", "class6", 
                                       "class5", "class4","class3","class2","class1"))
-    a$group <- a$group %>%  gsub(".[0-9]+_[0-9]+_","",.)
+    dat$group <- dat$group %>%  gsub(".[0-9]+_[0-9]+_","",.)
     
-    a <- mutate(a,group  = fct_relevel(group , "Flower_1" , "Flower_2" , "Cotyledon_1"   ,
+    dat <- mutate(dat,group  = fct_relevel(group , "Flower_1" , "Flower_2" , "Cotyledon_1"   ,
                                        "Cotyledon_2"  ,"Silique_1" ,"Silique_2",
                                        "Root_1"  ,"Root_2"   ,  "Stem_1"   , "Stem_2" ))
-    
-    a_up <- a[a$class=="up",]
-    a_down <- a[a$class=="down",]
+  
     
     col_draw <- colorspace::diverge_hcl(200,palette = "Blue-Red3",rev = F)#
-    p <- ggplot(data = a,mapping = aes(x=group,y = as.factor(motif),fill=log2(value)))+
+    plot_RNA <- ggplot(data = dat,mapping = aes(x=group,y = as.factor(motif),fill=log2(value)))+
       geom_tile()+ 
       coord_fixed(ratio = 1)+
       scale_fill_gradientn(colors = col_draw,name = "log2 Enrichment",limits=c(-1,1))+
@@ -86,14 +106,18 @@
         axis.title.y = element_blank(),
         plot.title =element_text(size=15)
       )    +labs(title = "10%  ")
-    p
+
     
-  }
-  ##3G ATI####
+  
+
+#####################################################################
+##3G ATI####
+#####################################################################
   {
+
+    list.files("jdk_data/1_ribosome/data2_GSE152938_kidney/",pattern = "gz$") %>% as.data.frame() %>% `colnames<-`(c(""))
     file <- read.table("~/diff_tissues_ATI.txt",header = TRUE)
     file$tissue <- rename_duplicates(file$tissue)
-    # file <- file[-c(17:22),]
     cl=makeCluster(21)
     registerDoParallel(cl)
     result <- list()
@@ -123,16 +147,13 @@
     
     save(result,file = "~/diff_tissue_ATI.Rdata")
   }
-  
-  
-  load("~/diff_tissue_ATI.Rdata")
-  # for (i in seq_along(result)) {result[[i]]$value <- log2(result[[i]]$value) }
+
   result <- bind_rows(result)
   result$value <- log2(result$value)
   result <- mutate(result,motif = fct_relevel(motif, "class11", "class10", "class9", 
                                               "class8", "class7", "class6", 
                                               "class5", "class4","class3","class2","class1"))
-  # result <- result[-grep("shoot|PSB|callus",result$group),]
+
   result <- mutate(result,group = fct_relevel(group,
                                               "flower_1"   , "flower_2"    ,"flower_3"   ,"flower_4"    ,"flower_5" ,
                                               "cotyledon_1", "cotyledon_2" ,"cotyledon_3", "cotyledon_4",
@@ -141,7 +162,7 @@
                                               "stem_1"     , "stem_2"      ,"stem_3"     ,"stem_4"))
   
   col_draw <- colorspace::diverge_hcl(200,palette = "Blue-Red3",rev = T)#
-  plot2 <- ggplot(data = result,mapping = aes(x=group,y = as.factor(motif),fill=value))+
+  plot_ATI <- ggplot(data = result,mapping = aes(x=group,y = as.factor(motif),fill=value))+
     geom_tile()+ 
     coord_fixed(ratio = 1)+
     scale_fill_gradientn(colors =  col_draw %>% rev(),name = "log2 Enrichment",limits=c(-5.1,5.1))+
@@ -152,19 +173,20 @@
       axis.title.y = element_blank(),
       plot.title =element_text(size=15)
     )    +labs(title = "ATI pseudo=1000 ")
-  plot2
+  
 
 
+#####################################################################
+##3G ATAC####
+#####################################################################
 
-
-  ##3G ATAC####
   {
+    #load file
     file <- list.files("~/ATAC/pre_processed/peak_calling/narrow",pattern = "bed$") %>% 
       paste0("~/ATAC/pre_processed/peak_calling/narrow",.) %>% as.data.frame()
     
     file$"tissues" <- file$. %>% basename() %>% gsub("_summits.bed","",.) %>% gsub("^.*_[0-9]+_","",.)
     colnames(file)[1] <- "file"
-    load("~/8_motif_net.Rdata")
     pwm.l <- read_jaspar("~/class11_pwm.txt")
     pwm.l <- convert_motifs(motifs = pwm.l,class ="TFBSTools-PWMatrix" ) 
     for (i in 1:length(pwm.l)) {names(pwm.l)[i] <- pwm.l[[i]]@name    }
@@ -192,11 +214,11 @@
       
       seqlevels(bed, pruning.mode="coarse") <- seqlevels(BSgenome.Athaliana.TAIR.TAIR9::Athaliana)
       seq <- BSgenome::getSeq(BSgenome.Athaliana.TAIR.TAIR9::Athaliana,bed)
-      df <- motifmatchr::matchMotifs(pwms = pwm,subject = seq,out ="matches",p.cutoff=5e-05,bg="subject")
+      df <- motifmatchr::matchMotifs(pwms = pwm,subject = seq,out ="matches",p.cutoff=1e-05,bg="subject")
       match <- df@assays@data@listData$motifMatches %>% colSums()
 
-      match_s <- lapply(1:10, function(x){
-        df_s <- motifmatchr::matchMotifs(pwms = pwm,subject = seq %>% universalmotif::shuffle_sequences(),out ="matches",p.cutoff=5e-05,bg="subject")
+      match_s <- lapply(1:length(pwm), function(x){
+        df_s <- motifmatchr::matchMotifs(pwms = pwm,subject = seq %>% universalmotif::shuffle_sequences(),out ="matches",p.cutoff=1e-05,bg="subject")
         match_s <- df_s@assays@data@listData$motifMatches %>% colSums()
         match_s
       }) %>% largeListToDf() %>% rowMeans()
@@ -207,12 +229,8 @@
     }
     stopImplicitCluster()
     stopCluster(cl)
-    
-    save(result,file = "~/diff_tissue_ATAC.Rdata")
   }
-
-
- 
+ #plot
   result <- bind_rows(result) 
   shuff_result <- bind_rows(shuff_result)
   shuff_result$group %<>%  paste0("Shuffle_",.)
