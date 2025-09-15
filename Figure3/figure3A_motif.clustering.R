611eacb0925582c67b1bdd76c0dd7bf2459bc04e
JIANGDK <jiangdk_bio@163.com>
2025-09-15 22:07:35 +0800

    Update and rename figure3A.R to figure3A_motif.clustering.R

--- Figure3/figure3A_motif.clustering.R
+++ Figure3/figure3A_motif.clustering.R
@@ -1,8 +1,11 @@
+##########################################################################
+#Figure3A compares, clusters and plots all monomer motifs
+##########################################################################
 pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
-  
+  #矩阵比对
   pwmlist_alig <- DNAmotifAlignment(threshold = 0,pfms = pwmlist %>% convert_motifs(class = "motifStack-pfm"),
                                     revcomp= rep(FALSE, length(pwmlist)))# %>% convert_motifs("TFBSTools-PWMatrix") %>% TF_logo.2(outfile = "2.txt.pdf")
-
+  #裁切矩阵保留核心区域使其等长
   for (i in 1:length(pwmlist_alig)) {
     pwmlist_alig[[i]]@mat <- pwmlist_alig[[i]]@mat[,-c(1:2,16:17)]
   }
@@ -12,7 +15,7 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
     names(c)[i] <- c[[i]]@name 
     
   }
-
+  #一维展开矩阵并计算距离进行聚类
   p <- data.frame()
   for (i in 1:length(c)) {
     p1 <- cbind(c[[i]]@mat[1,] %>% t(),c[[i]]@mat[2,] %>% t(),c[[i]]@mat[3,] %>% t(),c[[i]]@mat[4,] %>% t()) %>% t()
@@ -27,7 +30,7 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
   hc <- do.call("hclust", list(d = a))
   library(ggtree)
   phylog <- ade4::hclust2phylog(hc,add.tools = TRUE)
-  # plot(phylog,size=0.4)
+
   p <- ggtree(phylog,layout = "circular",size=0.6)  +
     geom_text(aes(label=node),size=1.5)+
     geom_tiplab(hjust=-0.5,size=4.2,align = TRUE,offset = 0)
@@ -57,6 +60,7 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
   text1 <-paste0("geom_strip(\"", p1, "\", \"",p1,"\", offset = 0.06, barsize = 20, extend = 0.6, color = \"#00BFC4\", offset.text = 3)") %>% paste0(collapse = "+")#offset :颜色bar 的外移内移
   text2 <-paste0("geom_strip(\"", p2, "\", \"",p2,"\", offset = 0.06, barsize = 20, extend = 0.6, color = \"#F8766D\", offset.text = 3)") %>% paste0(collapse = "+")
   {
+    #根据高度0.37进行砍树，将结果分为11类
     tree <- cutree(tree = hc,h = 0.37)
     class1 <- tree[tree==1] %>% names()
     class2 <- tree[tree==2] %>% names()
@@ -69,7 +73,7 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
     class9 <- tree[tree==9] %>% names()
     class10 <- tree[tree==10] %>% names()
     class11 <- tree[tree==11] %>% names()
-    # class12 <- tree[tree==12] %>% names()
+
     class1  <- paste0("geom_strip(\"", class1 , "\", \"",class1 ,"\", offset = 0.15, barsize = 10, extend = 0.6, color = \"#FFFFB3\",angle = 10,hjust = \"center\",fontsize = 4, offset.text = 2)") %>% paste0(collapse = "+")
     class2  <- paste0("geom_strip(\"", class2 , "\", \"",class2 ,"\", offset = 0.15, barsize = 10, extend = 0.6, color = \"#E5C494\",angle = 10,hjust = \"center\",fontsize = 4, offset.text = 2)") %>% paste0(collapse = "+")
     class3  <- paste0("geom_strip(\"", class3 , "\", \"",class3 ,"\", offset = 0.15, barsize = 10, extend = 0.6, color = \"#B3DE69\",angle = 10,hjust = \"center\",fontsize = 4, offset.text = 2)") %>% paste0(collapse = "+")
@@ -85,7 +89,7 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
   }
   
   plot_text=paste0("p + ",text1,"+",text2,"+",class1,"+",class2,"+",class3,"+",
-                   class4,"+",class5,"+",class6,"+",class7,"+",class8,"+",class9,"+",class10,"+",class11)#,"+",class12
+                   class4,"+",class5,"+",class6,"+",class7,"+",class8,"+",class9,"+",class10,"+",class11)
   plot <-  eval(parse(text = plot_text))
   
 
@@ -96,7 +100,7 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
     starstroke=0,
     starshape = 13,
     offset =0.24,
-    pwidth=0.30#轨道之间距离
+    pwidth=0.30
   )+
     scale_size_continuous(range=c(0, 4),
                           limits=c(min(dt$size), max(dt$size)),breaks=c(1, 2, 3))+
@@ -112,7 +116,7 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
 
 
 
-#生成11类代表性motif矩阵
+#根据树的聚类结果生成11类代表性motif矩阵
     class1 <- tree[tree==1] %>% names()
     class2 <- tree[tree==2] %>% names()
     class3 <- tree[tree==3] %>% names()
@@ -126,11 +130,9 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
     class11 <- tree[tree==11] %>% names()
   
   class <- list(class1,class3,class4,class2,class7,class6,class11,class10,class9,class8,class5)
-  
+  #生成代表性motif，平均IC大于0.5的，赋0.8的权重，小于0.5的赋0.2的权重
   pwm.l <- lapply(1:length(class), function(i){
     c1 <- c[class[[i]]] %>% convert_motifs("universalmotif-universalmotif")
-    # c1 <- c1[which(sapply(c1, function(x){ (x@icscore )/ncol(x@motif)})>0.6)]
-    
    for(x in 1:length(c1)){ if( 
      ((c1[[x]]@icscore )/ncol(c1[[x]]@motif) ) >0.5 ){
      c1[[x]]@motif <- c1[[x]]@motif *0.8
@@ -141,5 +143,6 @@ pwmlist <- read_jaspar("~/ALL_mono_P_S_T.pwm")
     a3@name <- glue("class{i}")
     a3
   })
-    write_jaspar(motifs = pwm.l,file = "2024_WRKY/8_motif_net/class11_pwm.txt",overwrite = T)
+
+    write_jaspar(motifs = pwm.l,file = "~/class11_pwm.txt",overwrite = T)
     
